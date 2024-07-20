bc9139090199e9f17bb5044a5a66e77e7ddb9d21
Moh Dwi Ramdhani <mohdwiramdhani@gmail.com>
2024-07-21 06:15:05 +0800

    Add unit test for get user API

--- test/user.test.js
+++ test/user.test.js
@@ -137,3 +137,32 @@ describe('POST /api/users/login', function () {
         expect(result.body.errors).toBeDefined();
     });
 });
+
+describe('GET /api/users/current', function () {
+    beforeEach(async () => {
+        await createTestUser();
+    });
+
+    afterEach(async () => {
+        await removeTestUser();
+    });
+
+    it('should can get current user', async () => {
+        const result = await supertest(web)
+            .get('/api/users/current')
+            .set('Authorization', 'test');
+
+        expect(result.status).toBe(200);
+        expect(result.body.data.username).toBe('dhani');
+        expect(result.body.data.name).toBe('dhani');
+    });
+
+    it('should reject if token is invalid', async () => {
+        const result = await supertest(web)
+            .get('/api/users/current')
+            .set('Authorization', 'salah');
+
+        expect(result.status).toBe(401);
+        expect(result.body.errors).toBeDefined();
+    });
+});
