adf658037b4aa455f20b5303a24af358a68791d7
Moh Dwi Ramdhani <mohdwiramdhani@gmail.com>
2024-07-21 00:35:56 +0800

    Add test utility

---
+++ test/test-util.js
@@ -0,0 +1,21 @@
+import { prismaClient } from "../src/application/database.js";
+import bcrypt from "bcrypt";
+
+export const removeTestUser = async () => {
+    await prismaClient.user.deleteMany({
+        where: {
+            username: "dhani"
+        }
+    })
+}
+
+export const createTestUser = async () => {
+    await prismaClient.user.create({
+        data: {
+            username: "dhani",
+            password: await bcrypt.hash("pw", 10),
+            name: "dhani",
+            token: "test"
+        }
+    })
+}
