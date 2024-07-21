6f8688a8f10cbb14cf4fbb29e34e58260a673b22
Moh Dwi Ramdhani <mohdwiramdhani@gmail.com>
2024-07-21 08:19:08 +0800

    Add route and controller for logout user API

--- src/route/api.js
+++ src/route/api.js
@@ -8,6 +8,7 @@ userRouter.use(authMiddleware);
 // User API
 userRouter.get('/api/users/current', userController.get);
 userRouter.patch('/api/users/current', userController.update);
+userRouter.delete('/api/users/logout', userController.logout);
 
 export {
     userRouter
