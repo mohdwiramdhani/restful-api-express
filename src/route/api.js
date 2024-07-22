309b72cfde7e78ad6fc0026cf6bd7b14f02a164b
Moh Dwi Ramdhani <mohdwiramdhani@gmail.com>
2024-07-23 00:57:55 +0800

    Add route and controller for create address API

--- src/route/api.js
+++ src/route/api.js
@@ -1,6 +1,7 @@
 import express from "express";
 import userController from "../controller/user-controller.js";
 import contactController from "../controller/contact-controller.js";
+import addressController from "../controller/address-controller.js";
 import { authMiddleware } from "../middleware/auth-middleware.js";
 
 const userRouter = new express.Router();
@@ -18,6 +19,8 @@ userRouter.put('/api/contacts/:contactId', contactController.update);
 userRouter.delete('/api/contacts/:contactId', contactController.remove);
 userRouter.get('/api/contacts', contactController.search);
 
+// Address API
+userRouter.post('/api/contacts/:contactId/addresses', addressController.create);
 
 export {
     userRouter
