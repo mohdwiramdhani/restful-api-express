82b94e9439d37628e903199efbb4a3622a5268f4
Moh Dwi Ramdhani <mohdwiramdhani@gmail.com>
2024-07-22 14:11:39 +0800

    Add service logic for get contact API

--- src/service/contact-service.js
+++ src/service/contact-service.js
@@ -1,6 +1,6 @@
 import { validate } from "../validation/validation.js";
 import {
-    createContactValidation
+    createContactValidation, getContactValidation
 } from "../validation/contact-validation.js";
 import { prismaClient } from "../application/database.js";
 
@@ -20,6 +20,31 @@ const create = async (user, request) => {
     });
 }
 
+const get = async (user, contactId) => {
+    contactId = validate(getContactValidation, contactId);
+
+    const contact = await prismaClient.contact.findFirst({
+        where: {
+            username: user.username,
+            id: contactId
+        },
+        select: {
+            id: true,
+            first_name: true,
+            last_name: true,
+            email: true,
+            phone: true
+        }
+    });
+
+    if (!contact) {
+        throw new ResponseError(404, "contact is not found");
+    }
+
+    return contact;
+}
+
 export default {
-    create
+    create,
+    get
 }
