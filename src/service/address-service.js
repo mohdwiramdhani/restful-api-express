a7840a9eb8251b1db15d07c246b0fc292eb40fec
Moh Dwi Ramdhani <mohdwiramdhani@gmail.com>
2024-07-23 01:24:15 +0800

    Add service logic for update address API

--- src/service/address-service.js
+++ src/service/address-service.js
@@ -1,9 +1,9 @@
 import { prismaClient } from "../application/database.js";
 import { validate } from "../validation/validation.js";
-import { getContactValidation } from "../validation/contact-validation.js";
+import { getContactValidation, } from "../validation/contact-validation.js";
 import { ResponseError } from "../error/response-error.js";
 import {
-    createAddressValidation, getAddressValidation
+    createAddressValidation, getAddressValidation, updateAddressValidation
 } from "../validation/address-validation.js";
 
 const checkContactMustExists = async (user, contactId) => {
@@ -68,7 +68,45 @@ const get = async (user, contactId, addressId) => {
     return address;
 }
 
+const update = async (user, contactId, request) => {
+    contactId = await checkContactMustExists(user, contactId);
+    const address = validate(updateAddressValidation, request);
+
+    const totalAddressInDatabase = await prismaClient.address.count({
+        where: {
+            contact_id: contactId,
+            id: address.id
+        }
+    });
+
+    if (totalAddressInDatabase !== 1) {
+        throw new ResponseError(404, "address is not found");
+    }
+
+    return prismaClient.address.update({
+        where: {
+            id: address.id
+        },
+        data: {
+            street: address.street,
+            city: address.city,
+            province: address.province,
+            country: address.country,
+            postal_code: address.postal_code,
+        },
+        select: {
+            id: true,
+            street: true,
+            city: true,
+            province: true,
+            country: true,
+            postal_code: true
+        }
+    })
+}
+
 export default {
     create,
-    get
+    get,
+    update
 }
