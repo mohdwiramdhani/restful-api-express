056734988f760e47dd24ffa187e186c271d548ad
Moh Dwi Ramdhani <mohdwiramdhani@gmail.com>
2024-07-22 19:42:36 +0800

    Add service logic for update contact API

--- src/service/contact-service.js
+++ src/service/contact-service.js
@@ -1,6 +1,6 @@
 import { validate } from "../validation/validation.js";
 import {
-    createContactValidation, getContactValidation
+    createContactValidation, getContactValidation, updateContactValidation
 } from "../validation/contact-validation.js";
 import { prismaClient } from "../application/database.js";
 
@@ -44,7 +44,43 @@ const get = async (user, contactId) => {
     return contact;
 }
 
+const update = async (user, request) => {
+    const contact = validate(updateContactValidation, request);
+
+    const totalContactInDatabase = await prismaClient.contact.count({
+        where: {
+            username: user.username,
+            id: contact.id
+        }
+    });
+
+    if (totalContactInDatabase !== 1) {
+        throw new ResponseError(404, "contact is not found");
+    }
+
+    return prismaClient.contact.update({
+        where: {
+            id: contact.id
+        },
+        data: {
+            first_name: contact.first_name,
+            last_name: contact.last_name,
+            email: contact.email,
+            phone: contact.phone,
+        },
+        select: {
+            id: true,
+            first_name: true,
+            last_name: true,
+            email: true,
+            phone: true
+        }
+    })
+}
+
+
 export default {
     create,
-    get
+    get,
+    update
 }
