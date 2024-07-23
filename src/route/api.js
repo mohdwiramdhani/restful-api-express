c9203e530d9b5640adedcd129df6dd03ba347b9d
Moh Dwi Ramdhani <mohdwiramdhani@gmail.com>
2024-07-24 07:17:45 +0800

    Remove logout endpoint from User API and controller

--- src/route/api.js
+++ src/route/api.js
@@ -10,7 +10,6 @@ userRouter.use(authMiddleware);
 // User API
 userRouter.get('/api/users/current', userController.get);
 userRouter.patch('/api/users/current', userController.update);
-userRouter.delete('/api/users/logout', userController.logout);
 
 // Contact API
 userRouter.post('/api/contacts', contactController.create);
