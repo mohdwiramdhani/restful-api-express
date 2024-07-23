1f5ace9565786ef8ee816761cb4adc985d8cdab7
Moh Dwi Ramdhani <mohdwiramdhani@gmail.com>
2024-07-24 04:17:38 +0800

    Refactor authentication to use JWT

--- src/middleware/auth-middleware.js
+++ src/middleware/auth-middleware.js
@@ -1,4 +1,4 @@
-import { prismaClient } from "../application/database.js";
+import jwt from "jsonwebtoken";
 
 export const authMiddleware = async (req, res, next) => {
     const token = req.get('Authorization');
@@ -7,18 +7,17 @@ export const authMiddleware = async (req, res, next) => {
             errors: "Unauthorized"
         }).end();
     } else {
-        const user = await prismaClient.user.findFirst({
-            where: {
-                token: token
+        jwt.verify(token, 'secret_key', (err, decoded) => {
+            if (err) {
+                return res.status(401).json({
+                    errors: "Unauthorized"
+                });
             }
-        });
-        if (!user) {
-            res.status(401).json({
-                errors: "Unauthorized"
-            }).end();
-        } else {
-            req.user = user;
+
+            const username = decoded.username;
+            req.user = { username: username };
+
             next();
-        }
+        });
     }
 }
--- src/service/user-service.js
+++ src/service/user-service.js
@@ -2,8 +2,8 @@ import { validate } from "../validation/validation.js";
 import { registerUserValidation, loginUserValidation, getUserValidation, updateUserValidation } from "../validation/user-validation.js";
 import { prismaClient } from "../application/database.js";
 import { ResponseError } from "../error/response-error.js";
+import jwt from "jsonwebtoken";
 import bcrypt from "bcrypt";
-import { v4 as uuid } from "uuid";
 
 const register = async (request) => {
     const user = validate(registerUserValidation, request);
@@ -51,18 +51,13 @@ const login = async (request) => {
         throw new ResponseError(401, "Username or password wrong");
     }
 
-    const token = uuid().toString()
-    return prismaClient.user.update({
-        data: {
-            token: token
-        },
-        where: {
-            username: user.username
-        },
-        select: {
-            token: true
-        }
-    });
+    const token = jwt.sign({ username: user.username }, 'secret_key', { expiresIn: '5h' });
+
+    return {
+        token,
+        username: user.username
+    };
+
 }
 
 const get = async (username) => {
