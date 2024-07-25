3e4ed9a97898fc87db5b8302b383b07d06bf98ae
Moh Dwi Ramdhani <mohdwiramdhani@gmail.com>
2024-07-26 01:24:10 +0800

    Add helmet and cors

--- src/application/web.js
+++ src/application/web.js
@@ -1,10 +1,15 @@
 import express from "express";
+import helmet from "helmet";
+import cors from "cors";
 import { publicRouter } from "../route/public-api.js";
 import { errorMiddleware } from "../middleware/error-middleware.js";
 import { userRouter } from "../route/api.js";
 
 export const web = express();
+
+web.use(helmet());
 web.use(express.json());
+web.use(cors());
 
 web.use(publicRouter);
 web.use(userRouter);
